46dd87245b2dca47dcccab49952af75ec1354d30
Zoltán Kádár <zoltan.kadar1@gmail.com>
2021-03-02 12:08:44 +0100

    update Pagination.jsx, App.js

--- frontend/star-wars/src/App.js
+++ frontend/star-wars/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import "./App.css";
 import React from "react";
-import IndexPage from './components/IndexPage/IndexPage.jsx';
-import NavigationBar from './components/NavigationBar/NavigationBar';
+import IndexPage from "./components/IndexPage/IndexPage.jsx";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+//import Pagination from './components/Pagination/Pagination.jsx';
 
 function App() {
   return (
     <div className="App">
-			<NavigationBar />
-			<IndexPage />
+      <NavigationBar />
+      <IndexPage />
     </div>
   );
 }
