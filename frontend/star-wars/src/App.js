bc121dc01cec6d91e88ea27067ea16d98d268485
Zoltán Kádár <zoltan.kadar1@gmail.com>
2021-03-03 14:33:32 +0100

    update

--- frontend/star-wars/src/App.js
+++ frontend/star-wars/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import React from "react";
 import IndexPage from "./components/IndexPage/IndexPage.jsx";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-//import Pagination from './components/Pagination/Pagination.jsx';
 
 function App() {
   return (
