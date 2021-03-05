afc72a0ad8a38f10d5eaea3e46822f88f497c995
RekaKR <RekaKR@users.noreply.github.com>
2021-03-05 18:38:41 +0100

    Reorganize files

--- frontend/star-wars/src/App.js
+++ frontend/star-wars/src/App.js
@@ -3,13 +3,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import IndexPage from './components/IndexPage/IndexPage.jsx';
 import NavigationBar from './components/NavigationBar/NavigationBar';
-//import Pagination from './components/Pagination/Pagination.jsx';
 import Login from './components/Login/Login';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Register from './components/Register/Register';
 import Logout from './components/Logout/Logout';
 import User from './components/User/User';
 import Voting from './components/Voting/Voting';
+
 function App() {
   const [response, setResponse] = useState('');
 
@@ -20,7 +20,7 @@ function App() {
       url: 'http://localhost:8000/home',
     }).then((res) => setResponse(res.data.username));
   });
-  console.log('App response logging: ', response);
+
   return (
     <Router>
       <div className='App'>
